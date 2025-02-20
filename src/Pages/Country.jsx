04b00ff3b90d817ce5c0787c1f6561214bc0b089
shaan Yadav <shaanyworkspace@gmail.com>
2025-02-21 00:31:54 +0530

    l

--- src/Pages/Country.jsx
+++ src/Pages/Country.jsx
@@ -1,81 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import { getCountryData } from '../API/postAPI'
+import React, { useEffect, useState } from 'react';
+import { getCountryData } from '../API/postAPI';
 import { useTransition } from 'react';
 import Loader from '../Components/UI/Loader';
 import CountryCard from '../Components/Layout/countryCard';
-
 import SearchFilter from '../Components/UI/SearchFilter';
-<<<<<<< HEAD
 import { searchFilter } from '../Components/Helper/helperFuntion';
-
-=======
->>>>>>> 2c48cbecb3025abf171f4a2abd2c4a016be6b3e4
 
 const Country = () => {
     const [isPending, startTransition] = useTransition();
     const [countries, setCountries] = useState([]);
-<<<<<<< HEAD
     const [search, setSearch] = useState("");
     const [filter, setFilter] = useState("All");
-=======
-    const [search , setSearch] = useState("");
-    const [filter , setFilter] = useState("All");
-
->>>>>>> 2c48cbecb3025abf171f4a2abd2c4a016be6b3e4
 
     useEffect(() => {
         startTransition(async () => {
             const res = await getCountryData();
             setCountries(res.data);
-        })
-    }, [])
+        });
+    }, []);
 
-<<<<<<< HEAD
-    if (isPending) return <Loader />
-=======
-    //Until Function is loading , this will keep displaying the loader class
-    if (isPending) return <Loader />
-    
-    
-    // Search function
-    const searchCountry= (country)=>{
-        if(search){
-            return country.name.common.toLowerCase().includes(search.toLowerCase());
-        }
-        return country
-    }
-    const filteredCountries = countries.filter((country)=>searchCountry(country))
-    
->>>>>>> 2c48cbecb3025abf171f4a2abd2c4a016be6b3e4
+    // Until data is loading, show the Loader component
+    if (isPending) return <Loader />;
 
+    // Step 1: Search Function
     const searchedCountries = searchFilter(countries, search);
+
+    // Step 2: Region Filtering Function
     const filterRegion = (country) => {
-        if (filter === "All") return true; 
+        if (filter === "All") return true;
         return country.region === filter;
     };
+
+    // Step 3: Apply Region Filtering
     const filteredCountries = searchedCountries.filter(filterRegion);
+
     return (
-        <div className='mt-20 flex justify-center items-center '>
+        <div className='mt-20 flex justify-center items-center'>
             <div className='w-[80%]'>
-<<<<<<< HEAD
-                <SearchFilter search={search} setSearch={setSearch} filter={filter} setFilter={setFilter} Data={countries} setData={setCountries}/>
-=======
-                <SearchFilter search={search} setSearch={setSearch} filter={filter} setFilter={setFilter}/>
->>>>>>> 2c48cbecb3025abf171f4a2abd2c4a016be6b3e4
-                <div id='Tasklist' className=' mt-3 h-screen  flex  flex-nowrap overflow-y-auto  justify-star'>
-                    <ul className=' mt-5 flex flex-wrap gap-2 justify-evenly items-center'>
+                <SearchFilter search={search} setSearch={setSearch} filter={filter} setFilter={setFilter} />
+                <div id='Tasklist' className='mt-3 h-screen flex flex-nowrap overflow-y-auto justify-start'>
+                    <ul className='mt-5 flex flex-wrap gap-2 justify-evenly items-center'>
                         {
-                            filteredCountries.map((currCountry, idx) => {
-                                return <>
-                                    <CountryCard country={currCountry} key={idx} />
-                                </>
-                            })
+                            filteredCountries.map((currCountry, idx) => (
+                                <CountryCard country={currCountry} key={idx} />
+                            ))
                         }
                     </ul>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Country
+export default Country;
